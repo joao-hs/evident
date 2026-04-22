5a316162cbca58f7369d78ce0927439b7209214c
João Sereno <joaohsereno@tecnico.ulisboa.pt>
2026-04-22 12:05:30 +0100

    fix instructions
    Signed-off-by: João Sereno <joaohsereno@tecnico.ulisboa.pt>

--- dev.lima/create.sh
+++ dev.lima/create.sh
@@ -8,7 +8,7 @@ LIMA_YAML_TMPL=$(realpath $SRCDIR/config/lima.yaml.j2)
 
 source $SRCDIR/env.sh
 
-jinja2 $LIMA_YAML_TMPL -o $LIMA_YAML -D project="$(realpath $SRCDIR/..)"
+jinja2 $LIMA_YAML_TMPL -o $LIMA_YAML -D project="$(realpath $SRCDIR/..)" -D arch="$(uname -m)"
 echo "Warning: provisioning scripts take around 10 minutes to complete"
 limactl create --name $VM_NAME $LIMA_YAML
 limactl start $VM_NAME
